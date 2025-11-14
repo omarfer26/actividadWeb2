80ec03e905cba59b3b6a4b937838668a5abe6104
omarfer26 <omarfernandoso@ufps.edu.co>
2025-11-14 10:49:54 -0500

    hola

---
+++ src/main/java/com/ufps/prueba/controllers/MvcProductoController.java
@@ -0,0 +1,15 @@
+package com.ufps.prueba.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/productos")
+@RequiredArgsConstructor
+public class MvcProductoController {
+    
+    
+
+}
